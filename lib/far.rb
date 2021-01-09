57caaf35fe092298f17a1e66ce14afbf8170915b
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-09 15:55:26 +0200

    done

--- lib/far.rb
+++ lib/far.rb
@@ -1,7 +1,11 @@
 class String
   def find_and_replace(pattern, replacement)
-    return self.split(' ')
-      .map {|w| w == pattern ? replacement : w}
-      .join(' ')
+    # Recursive version.  
+    
+    return self if self.length < pattern.length
+
+    return replacement + self[pattern.length..].find_and_replace(pattern, replacement) if self[0, pattern.length] == pattern
+
+    return self[0] + self[1..].find_and_replace(pattern, replacement)
   end
 end
--- spec/far_spec.rb
+++ spec/far_spec.rb
@@ -10,5 +10,13 @@ describe 'find_and_replace' do
   it 'does not replace anything if there is nothing to replace' do
     expect("Call me Ishmael.".find_and_replace("Ashot", "Kamala")).to eq("Call me Ishmael.")
   end
+
+  it 'replaces the pattern, whenever it is' do
+    expect("Gretha Thunberg".find_and_replace("Thun", "Nun")).to eq("Gretha Nunberg")
+  end
+
+  it 'replaces the pattern at the end' do
+    expect("gaba-gaba-gay".find_and_replace("gay", "sad")).to eq("gaba-gaba-sad")
+  end
 end
 
